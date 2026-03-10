b93e58d12edefd64f6891dea24a0d6dc2737621e
Wilmar <89958863+wgrojas@users.noreply.github.com>
2026-03-10 15:13:59 -0500

    first commit

--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -1,9 +1,37 @@
 const express = require("express");
 const router = express.Router();
+const db = require("../config/db");
+
 const { registerUser, loginUser } = require("../controllers/usuarioController");
 
-// Rutas
+// ====================
+// VER USUARIOS
+// ====================
+
+router.get("/", (req, res) => {
+
+  db.query("SELECT id, nombre FROM usuarios", (err, result) => {
+
+    if (err) {
+      return res.status(500).json({ error: err });
+    }
+
+    res.json(result);
+
+  });
+
+});
+
+// ====================
+// REGISTRO
+// ====================
+
 router.post("/register", registerUser);
+
+// ====================
+// LOGIN
+// ====================
+
 router.post("/login", loginUser);
 
 module.exports = router;
